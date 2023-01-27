4b8aa932622c5932efa55c4b5d82715cf85c6f59
Aaron MV <epsilon11101@gmail.com>
2023-01-27 08:26:46 -0600

    added responsive search

--- src/components/search.js
+++ src/components/search.js
@@ -13,41 +13,40 @@ class C_Search extends LitElement {
           align-items: center;
         }
         div {
-          width: 50%;
+          width: 70%;
           height: 30px;
           display: flex;
           align-items: center;
           padding: 10px;
+          border: 1px var(--white_primary);
+          border-style: none none solid none;
         }
         input {
           flex: 1;
-          height: 40px;
+          height: 100%;
           color: var(--white_primary);
           font-weight: var(--bold);
           font-size: 14px;
           outline: none;
           background-color: transparent;
-          border-top: none;
-          border-left: none;
-          border-right: none;
-          border-bottom: 1px solid var(--white_primary);
+          border: none;
         }
         ::slotted(i) {
           font-size: 20px;
           color: var(--white_primary);
         }
 
-        /* @media (max-width: 768px) {
-          input {
-            background-color: red;
+        @media (min-width: 767px) {
+          div {
+            width: 50%;
           }
         }
 
-        @media (min-width: 769px) {
-          input {
-            background-color: purple;
+        @media (min-width: 1024px) {
+          div {
+            width: 33%;
           }
-        } */
+        }
       `,
     ];
   }
