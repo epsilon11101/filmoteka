349d7151599786d653ca22964a7c3c3982e59e4f
Aaron MV <epsilon11101@gmail.com>
2023-01-27 08:27:48 -0600

    remove unnecessary files and assets and modify some files

---
+++ src/scripts/assets.js
@@ -0,0 +1,11 @@
+import desktop_user from "../assets/desktop_user.png";
+import desktop_main from "../assets/desktop_main.png";
+import icon from "../assets/film_icon.png";
+
+const images = {
+  desktop_user: desktop_user,
+  desktop_main: desktop_main,
+  icon: icon,
+};
+
+export default images;
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,57 +1,59 @@
 import "../css/utils/brand.css";
 import "../components/header";
-import "../components/modal";
-import MovieApi from "./movie_api";
+// import "../components/modal";
+// import MovieApi from "./movie_api";
 
-const API = new MovieApi();
+// const API = new MovieApi();
 
-// CONSULTAR TODAS LAS PELICULAS EN TENDENCIA
-API.query_params = "trending/movie/week";
-console.log(API.url);
+// // CONSULTAR TODAS LAS PELICULAS EN TENDENCIA
+// API.query_params = "trending/movie/week";
+// console.log(API.url);
 
-API.getAllData().then((movies) => {
-  console.log(movies.results);
-  console.log(API.get_data_keys(movies));
-  console.log(API.get_data_keys(movies.results[0]));
-});
+// API.getAllData().then((movies) => {
+//   console.log(movies.results);
+//   console.log(API.get_data_keys(movies));
+//   console.log(API.get_data_keys(movies.results[0]));
+// });
 
-// CONSULTAR PELICULAS QUE COINCIDAN CON ALGUNA BUSQUEDA
+// // CONSULTAR PELICULAS QUE COINCIDAN CON ALGUNA BUSQUEDA
 
-API.query_params = `search/movie`;
-console.log(API.url);
+// API.query_params = `search/movie`;
+// console.log(API.url);
 
-API.query = "robots"; //buscar peliculas de robots
-API.getAllData().then((movies) => {
-  console.log(movies);
-  console.log(API.get_data_keys(movies));
-  console.log(API.get_data_keys(movies.results[0]));
-});
-//RECUERDA BORRAR LA BUSQUEDA PARA NO TENER PROBLEMAS MAS ADELANTE
-API.query = "";
+// API.query = "robots"; //buscar peliculas de robots
+// API.getAllData().then((movies) => {
+//   console.log(movies);
+//   console.log(API.get_data_keys(movies));
+//   console.log(API.get_data_keys(movies.results[0]));
+// });
+// //RECUERDA BORRAR LA BUSQUEDA PARA NO TENER PROBLEMAS MAS ADELANTE
+// API.query = "";
 
-// CONSULTAR INFORMACION DE UNA SOLA PELICULA
-API.query_params = "movie/9928";
-API.getAllData().then((movie) => {
-  console.log(movie);
-  console.log(API.get_data_keys(movie));
-});
+// // CONSULTAR INFORMACION DE UNA SOLA PELICULA
+// API.query_params = "movie/9928";
+// API.getAllData().then((movie) => {
+//   console.log(movie);
+//   console.log(API.get_data_keys(movie));
+// });
 
-// OBTENER GENEROS O GENERO ESPECIFICO
+// // OBTENER GENEROS O GENERO ESPECIFICO
 
-API.get_Genre().then((data) => {
-  console.log(data);
-  console.log(data.get(878));
-});
+// API.get_Genre().then((data) => {
+//   console.log(data);
+//   console.log(data.get(878));
+// });
 
-// OBTENER EL NOMBRE DE UN GENERO
-API.get_Genre().then((dataG) => {
-  API.query_params = "trending/movie/week";
-  API.getAllData().then((data) => {
-    data.results.map((movie) => {
-      const { title, genre_ids } = movie;
-      console.log(
-        `Pelicula: ${title}, Generos: ${genre_ids.map((gen) => dataG.get(gen))}`
-      );
-    });
-  });
-});
+// // OBTENER EL NOMBRE DE UN GENERO
+// API.get_Genre().then((dataG) => {
+//   API.query_params = "trending/movie/week";
+//   API.getAllData().then((data) => {
+//     data.results.map((movie) => {
+//       const { title, genre_ids } = movie;
+//       console.log(
+//         `Pelicula: ${title}, Generos: ${genre_ids.map((gen) => dataG.get(gen))}`
+//       );
+//       console.log(genre_ids);
+//       console.log(genre_ids.map((gen) => dataG.get(gen)));
+//     });
+//   });
+// });
